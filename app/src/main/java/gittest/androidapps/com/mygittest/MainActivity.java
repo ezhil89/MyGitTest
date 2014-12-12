10d1c6673ea05ecbd0ec92e3289db99503593dcc
ezhil89 <ezhil.89@gmail.com>
2014-12-12 23:15:55 +0530

    commit test2

--- app/src/main/java/gittest/androidapps/com/mygittest/MainActivity.java
+++ app/src/main/java/gittest/androidapps/com/mygittest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // now testing the check in feature..
+    // now testing the check in feature12..
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
